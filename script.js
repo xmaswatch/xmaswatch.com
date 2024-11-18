73a392867299f48f5c81fb238ec3f7269c1be45b
xmaswatch <agacash@C17>
2024-11-18 21:37:44 +0100

    center numbers

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Countdown Timer
-function updateTimer() {
-    const christmas = new Date('Dec 25, 2024 00:00:00').getTime();
-    const now = new Date().getTime();
-    const distance = christmas - now;
-
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("timer").innerHTML = 
-        `${days} days&nbsp;&nbsp;&nbsp;${hours} hrs&nbsp;&nbsp;&nbsp;${minutes} mins&nbsp;&nbsp;&nbsp;${seconds} secs`;
-
-    if (distance < 0) {
-        document.getElementById("timer").innerHTML = "Merry Christmas!";
-    }
-}
-
-setInterval(updateTimer, 1000);
-
 // Social Media Sharing
 function shareOn(platform) {
     const url = "https://yourchristmascountdownsite.com";
